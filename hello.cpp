641582fd80ea2fd1adbf1a305ed84f4585cdda13
Zechariah Utecht <utechtzs@gmail.com>
2024-06-02 14:36:33 -0500

    comments

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,18 @@
+/*
+ * hello.cpp
+ * Prints out my name, hometown,
+ * and favorite hobby.
+ * by Zechariah
+ */
+
 #include <cstdio>
 
 int main()
 {
-  printf("Hello, my name is Zechariah.");
-  printf("Green Lake");
+  // prints out my name
+  printf("Hello, my name is Zechariah.\n");
+  // prints my hometown
+  printf("Green Lake\n");
+  // there is not beach in New Mexico!
   printf("Reading");
 }
